40fae9df44c6b856ed187d1b98aa935791fb1b9d
@CyberAndi <astawinski@outlook.com>
2023-10-23 20:37:31 +0200

    dashboard

--- openWRT22_install.sh
+++ openWRT22_install.sh
@@ -1157,15 +1157,16 @@ echo
 #sichere alte Konfiguration
 echo Sichere alte Konfiguration
 iptables-save > rules.v4_old_$datum.bkp
-rm /www/luci-static/bootstrap/cascade.css
 
 processes=$(rm /www/luci-static/bootstrap/c*.css)
+wait $processes
+processes=$(rm /www/luci-static/resources/view/dashboard/css/c*.css)
 wait $processes
 processes1=$(wget https://github.com/CyberAndi/CyberSecurity-Box/raw/CyberAndi-Pi-Hole-5/CyberSecurity-Box.png -P /www/luci-static/bootstrap/)
 wait $processes1
 processes1=$(wget https://github.com/CyberAndi/CyberSecurity-Box/raw/CyberAndi-Pi-Hole-5/custom.css -P /www/luci-static/bootstrap/)
 wait $processes1
-wget https://github.com/CyberAndi/CyberSecurity-Box/raw/CyberAndi-Pi-Hole-5/cascade.css -P /www/luci-static/bootstrap/
+wget https://github.com/CyberAndi/CyberSecurity-Box/raw/CyberAndi-Pi-Hole-5/cascade.css -P /www/luci-static/resources/view/dashboard/css/
 }
 
 create_websites() {
