800c96fc20fdc64b00378b00573b75f0f2c9f958
@CyberAndi <astawinski@outlook.com>
2023-12-08 16:24:19 +0100

    fix copy error

--- openWRT23_install.sh
+++ openWRT23_install.sh
@@ -1251,7 +1251,7 @@ DEVICE_REVISION='v0.75'
 
 EOF
 
-cp openWRT22_install.sh /etc/openWRT_install.sh
+cp openWRT*.sh /etc/openWRT_install.sh
 chmod 0755 /etc/openWRT_install.sh
 
 cat << EOF > /etc/sysupgrade.conf
