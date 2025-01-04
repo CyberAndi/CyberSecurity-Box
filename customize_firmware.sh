e68f5a7f3b973cbf4a4ebdb6537db86da9bb9918
@CyberAndi <programming@cyberandi.slmail.me>
2025-01-05 00:52:47 +0100

    test3

--- customize_firmware.sh
+++ customize_firmware.sh
@@ -113,8 +113,9 @@ check_download()  {
 
 
 	out=$(wget --waitretry=10 -t 5 -O "/root/$OUTPUT_FILE" "$URL")
-    
-	wait $out && if [[ $? -eq 0 ]]; then
+    wait $out
+	sleep 40
+	 if [[ $? -eq 0 ]]; then
     	if check_hash "$OUTPUT_FILE"; then
         	echo "Hash is okay"  >> /root/install_customice.log
         	break
